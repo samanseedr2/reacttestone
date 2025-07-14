3601f195eaa3f755b64362ecc1772a837a8d0080
SamanPrasad <samanseedr2@gmail.com>
2025-07-14 17:45:30 +0530

    base uri changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reacttestone",
+  base: import.meta.env.VITE_ENV == "gh" ? "/reacttestone" : "/",
   build: { outDir: "build" },
 });
