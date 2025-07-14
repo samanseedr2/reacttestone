5dbb669110a55b897f53ec18bebf469be72c2937
SamanPrasad <samanseedr2@gmail.com>
2025-07-14 22:02:48 +0530

    changes in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig(() => {
-  // const env = loadEnv(mode, process.cwd(), "");
-  const env = process.env;
+const env = process.env;
 
-  return {
-    plugins: [react()],
-    base: env.VITE_ENV == "gh" ? "/reacttestone" : "/",
-    build: { outDir: "build" },
-  };
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: env.VITE_ENV == "gh" ? "/reacttestone" : "/",
+  build: { outDir: "build" },
 });
