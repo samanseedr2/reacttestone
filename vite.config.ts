1ce831580d2952d575ef5591cb64714cb7ef1c91
SamanPrasad <samanseedr2@gmail.com>
2025-07-14 22:00:12 +0530

    changes in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  // const env = loadEnv(mode, process.cwd(), "");
+  const env = process.env;
 
   return {
     plugins: [react()],
